41ff118a0b452d9cb4a75901d7c45d8d872c6ffa
Sammy <skariuki@seaburysolutions.com>
2026-02-27 07:39:22 +0300

    Initial Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,29 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Here you would register your services with the dependency injection container.
+            // For example, if you're using Microsoft.Extensions.DependencyInjection:
+            // var services = new ServiceCollection();
+            // services.AddTransient<IMyService, MyService>();
+            // ... and so on for other services and repositories.
+
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+
+        }
+    }
+}
