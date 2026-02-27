d473d8d60ffc46d7b287c346ff6ca22f90b428be
Sammy <skariuki@seaburysolutions.com>
2026-02-27 08:34:22 +0300

    Re-arranged Banking Domain class Library and added db context for account

---
+++ MicroRabbit/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace MicroRabbit.Banking.Domain.Interfaces
+{
+	public interface IAccountRepository()
+	{
+		IEnumerable <Account> GetAccounts();
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDbContext: DbContext
+    {
+        public BankingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        // DbSet for Accounts
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
