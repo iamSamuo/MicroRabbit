a8553a4ddab0bf0b1a6315623610b5b9b9ada167
Sammy <skariuki@seaburysolutions.com>
2026-05-04 08:25:52 +0300

    Add transfer API

--- MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +24,13 @@ namespace MicroRabbit.Banking.Api.Controllers
                 return Ok(_accountService.GetAccounts());
 
             }
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            _accountService.Transfer(accountTransfer);
+
+            return Ok(accountTransfer);
+        }
 
     }
 }
---
+++ MicroRabbit/MicroRabbit.Banking.Application/Models/AccountTransfer.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MicroRabbit.Banking.Application.Models
+{
+    public class AccountTransfer
+
+    {
+        public int FromAccount { get; set; }
+        public int ToAccount { get; set; }
+        public decimal TranferAmount { get; set; }
+    }
+}
